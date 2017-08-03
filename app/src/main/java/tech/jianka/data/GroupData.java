f65c539e2f5ae56f191dacaf6485cf437e32092c
Richard <richardplus@foxmail.com>
2017-08-03 16:13:35 +0800

    添加卡片

---
+++ app/src/main/java/tech/jianka/data/GroupData.java
@@ -0,0 +1,25 @@
+package tech.jianka.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static tech.jianka.utils.CardUtil.getChildItems;
+import static tech.jianka.utils.CardUtil.getSpecifiedSDPath;
+
+/**
+ * Created by Richard on 2017/8/3.
+ */
+
+public class GroupData {
+    private List<Item> itemGroup = new ArrayList<>();
+
+    public GroupData() {
+        String path = getSpecifiedSDPath("jianka/data");
+        itemGroup = getChildItems(path);
+        itemGroup.add(new Item("任务", Item.GROUP));
+    }
+
+    public List<Item> getItemGroup() {
+        return itemGroup;
+    }
+}
